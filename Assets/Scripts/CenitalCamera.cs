16a23b9f0cbe7678bb6090deae4f7c80e01506b7
victorbr2 <47553336+victorbr2@users.noreply.github.com>
2025-02-21 12:10:34 +0100

    Menu and Camera

---
+++ Assets/Scripts/CenitalCamera.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class CamaraCenital : MonoBehaviour
+{
+    [SerializeField] private float zoomSpeed = 10f;  // Velocidad de zoom
+    [SerializeField] private float minZoom = 20f;    // Zoom mínimo
+    [SerializeField] private float maxZoom = 40f;    // Zoom máximo
+
+    private CinemachineCamera TopcinemachineCamera;  // Referencia a la cámara de Cinemachine
+
+    void Start()
+    {
+        TopcinemachineCamera = GetComponent<CinemachineCamera>(); // Obtener la cámara Cinemachine
+    }
+
+    void Update()
+    {
+        if (FindAnyObjectByType<CamerasController>().GetActiveCamera() == TopcinemachineCamera)
+        {
+            ZoomCamera();
+        }
+
+    }
+
+    void ZoomCamera()
+    {
+        if (TopcinemachineCamera == null) return;
+
+        // Control del zoom con la rueda del ratón
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+        if (scrollInput != 0f)
+        {
+            LensSettings lens = TopcinemachineCamera.Lens;
+            lens.FieldOfView -= scrollInput * zoomSpeed;
+            lens.FieldOfView = Mathf.Clamp(lens.FieldOfView, minZoom, maxZoom);
+            TopcinemachineCamera.Lens = lens; // Aplicar cambios
+        }
+
+
+        if (Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = currentMagnitude - prevMagnitude;
+
+            LensSettings lens = TopcinemachineCamera.Lens;
+            lens.FieldOfView -= difference * zoomSpeed * 0.01f;
+            lens.FieldOfView = Mathf.Clamp(lens.FieldOfView, minZoom, maxZoom);
+            TopcinemachineCamera.Lens = lens; // Aplicar cambios
+        }
+    }
+}
