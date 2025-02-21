2051988106e4b7a335f1a1838478f05afbd9125c
JavierMartinLCI <javier.martin@lcibarcelona.com>
2025-02-21 12:20:52 +0100

    movement proves

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -7,6 +7,7 @@ public class Unit : MonoBehaviour
     private Vector3 init;
     private Vector3 end;
     private float speed;
+    private float distance;
 
     private bool move = false;
 
@@ -17,8 +18,28 @@ public class Unit : MonoBehaviour
         this.init = init;
         this.end = end;
         this.speed = speed;
+        move = true;
+    }
 
+    private void Update()
+    {
+        if (move)
+        {
 
+            this.gameObject.transform.position = Vector3.Lerp(transform.position, end, speed * Time.deltaTime);
+            distance = Vector3.Distance(init, end);
+            if (distance <= 0.1f)
+            {
+                move = false;
+            }
+
+        }
+
+    }
+    private void Start()
+    {
+        
+        MoveUnit(transform.position, new Vector3(10,transform.position.y,0),1.0f);
     }
 
 }
