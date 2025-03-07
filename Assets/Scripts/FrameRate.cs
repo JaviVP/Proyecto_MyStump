aa18c1fbf2927707186c30bb2e863667abd54ae3
victorbr2 <47553336+victorbr2@users.noreply.github.com>
2025-03-07 12:22:01 +0100

    Cambios

--- Assets/Scripts/FrameRate.cs
+++ Assets/Scripts/FrameRate.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
-
+using TMPro;
 public class FrameRate : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI frames;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +13,7 @@ public class FrameRate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        frames.text = "Fps: " + Mathf.RoundToInt(1.0f / Time.deltaTime);
         Debug.Log("FPS: " + (1.0f / Time.deltaTime));
     }
 }
