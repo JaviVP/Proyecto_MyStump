a8f0efc0e7b55bf2ff82ea26d7ec8ccb259e3f64
JavierMartinLCI <javier.martin@lcibarcelona.com>
2025-03-18 13:31:29 +0100

    Update material tile Outline

--- Assets/Scripts/Grid/HexTile.cs
+++ Assets/Scripts/Grid/HexTile.cs
@@ -9,7 +9,7 @@ public class HexTile : MonoBehaviour
     private Renderer tileRenderer;
 
     [SerializeField] private Material highlightMaterial; // Assign in Inspector
-    private Material baseMaterial;
+    [SerializeField] private Material baseMaterial;
 
     private static readonly int ColorProperty = Shader.PropertyToID("_Color");
 
@@ -46,13 +46,33 @@ public class HexTile : MonoBehaviour
 
     public void HighlightTile()
     {
-        tileRenderer.material = highlightMaterial; // Apply the highlight shader
-        highlightMaterial.SetColor(ColorProperty, new Color(1f, 1f, 0f, 1f)); // Set highlight color (yellow)
+
+
+        //tileRenderer.material = highlightMaterial; // Apply the highlight shader
+        //highlightMaterial.SetColor(ColorProperty, new Color(1f, 1f, 0f, 1f)); // Set highlight color (yellow)
+        var materials = tileRenderer.materials;
+        Material[] newMaterials = new Material[materials.Length + 1];
+
+        for (int i = 0; i < materials.Length; i++)
+        {
+            newMaterials[i] = materials[i];
+        }
+        newMaterials[newMaterials.Length - 1] = highlightMaterial;
+
+        tileRenderer.materials = newMaterials;
+
     }
 
     public void ResetTileColor()
     {
-        tileRenderer.material = baseMaterial;
+        //tileRenderer.material = baseMaterial;
+        var materials = tileRenderer.materials;
+        if (materials.Length > 1)
+        {
+            tileRenderer.materials = new Material[] { materials[0] };
+        }
+       
+
         UpdateTileAppearance(); // Restore original tile material
     }
 }
