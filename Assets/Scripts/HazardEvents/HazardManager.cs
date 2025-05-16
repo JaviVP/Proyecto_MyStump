297b6e7808ef1428990d3002a33e5e59c01a5808
CoheteEspacial <77506448+CoheteEspacial@users.noreply.github.com>
2025-05-16 09:14:23 +0200

    Solucion conflictos

--- Assets/Scripts/HazardEvents/HazardManager.cs
+++ Assets/Scripts/HazardEvents/HazardManager.cs
@@ -11,9 +11,6 @@ public class HazardEventsManager : MonoBehaviour
     [SerializeField]
     private List<Hazard> hazardPool = new List<Hazard>();
 
-    Dictionary<int, List<Hazard>> hazardByTurn = new Dictionary<int, List<Hazard>>();
-
-
     private Dictionary<int, List<Hazard>> HazardByTurn = new Dictionary<int, List<Hazard>>();
 
 
@@ -24,36 +21,7 @@ public class HazardEventsManager : MonoBehaviour
     }
     public void ShuffleHazardPool()
     {
-
-        List<HazardOLD> hazardList = new List<HazardOLD>
-        {
-            new HazardCarreteraFantasma(),
-            new HazardCarreteraFantasma()
-            /*new HazardSequiaExtrema(),
-            new HazardDerramePetroleo(),
-            new HazardDisputaPorElCaucho(),
-            new HazardElPulsoDelRio(),
-            new HazardExpansionUrbana(),
-            new HazardMineriaMercurio(),
-            new HazardTormentaDelTropico()*/
-        };
-
-        // Mezclar la lista de hazards
         System.Random rng = new System.Random();
-        List<HazardOLD> shuffledHazards = hazardList.OrderBy(h => rng.Next()).ToList();
-        int turnCounter = 2;
-        int key = 0;
-        foreach (HazardOLD h in shuffledHazards)
-        {
-            EventHazard eh = new EventHazard();
-            eh.Hazard = h;
-            eh.Turn = turnCounter;
-            turnCounter+=2;
-            key++;
-            hazardDictionary.Add(key, eh);
-
-        System.Random rng = new System.Random();
-
 
         int n = hazardPool.Count;
         while (n > 1)
@@ -67,38 +35,14 @@ public class HazardEventsManager : MonoBehaviour
     }
 
 
-    public void CheckHazardEvents(int turn)
-    {
-        HazardOLD h = GetHazardByTurn(turn);
-        if (h!=null)
-        {
-            Debug.Log("Lanzo un Hazard ("+turn+")  Tipo: "+ h.GetType().ToString());
-            
-            h.Apply();
-            
-        }
-    }
 
-    public HazardOLD GetHazardByTurn(int turn)
-    {
-        HazardOLD h = null;
-        foreach (EventHazard kvp in hazardDictionary.Values)
-        {
-            if (kvp.Turn ==turn)
-            {
-                return kvp.Hazard;
-            }
-            //Console.WriteLine("Value:" + kvp.Hazard.Description);
-        }
-        return h;
-    }
 
 
     public void TurnAssignation()
     {
         /// Write those out ffrom here
-       // int maxTurns = 40; //No se usa de momento lo comento
-       // int basicProbability = 25; //No se usa de momento lo comento
+        // int maxTurns = 40; //No se usa de momento lo comento
+        // int basicProbability = 25; //No se usa de momento lo comento
         int currentProbability = 25;
         int maxProbability = 70;
         int basicAdditive = 5;
